66fb8dbc9b53af04b49424ad2fbabd3f4b59f9ff
Yogesh Krishnan <yogeshsuresh28@gmail.com>
2023-07-04 18:04:00 +0530

    Completed website

---
+++ Contact/form.js
@@ -0,0 +1,53 @@
+function validate() {
+  var name = document.getElementById("name").value;
+  // var subject = document.getElementById("subject").value;
+  var phone = document.getElementById("phone").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("error_message");
+
+  error_message.style.padding = "10px";
+
+  var text;
+  if (name.length < 5) {
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  // if (subject.length < 10) {
+  //   text = "Please Enter Correct Subject";
+  //   error_message.innerHTML = text;
+  //   return false;
+  // }
+  if (isNaN(phone) || phone.length != 10) {
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if (email.indexOf("@") == -1 || email.length < 6) {
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  // if (message.length <= 140) {
+  //   text = "Please Enter More Than 140 Characters";
+  //   error_message.innerHTML = text;
+  //   return false;
+  // }
+
+  var request = new XMLHttpRequest();
+  request.open("POST", "https://hook.integromat.com/9eihytj3ukms4xt7i3ibmhht32r6vsr5");
+  request.setRequestHeader('Content-type', 'application/json');
+
+  var params = {
+    name,
+    phone,
+    email,
+    message
+}
+
+  request.send(JSON.stringify(params));
+
+  alert("Form Submitted Successfully!");
+  return true;
+}
